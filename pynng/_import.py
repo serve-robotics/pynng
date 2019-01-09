96baa551ec9302760b153e18752c6b4ddd279f98
Cody Piersall <cody.piersall@gmail.com>
2019-01-08 20:17:27 -0600

    Pass correct name to the extension module.

--- pynng/_import.py
+++ pynng/_import.py
@@ -5,7 +5,7 @@ import os
 import pynng._nng
 this_dir = os.path.dirname(os.path.abspath(__file__))
 path = pynng._nng.__file__
-name = '_nng_extra'
+name = 'greet'
 loader = importlib.machinery.ExtensionFileLoader(name, path)
 spec = importlib.util.spec_from_loader(name, loader)
 module = importlib.util.module_from_spec(spec)
